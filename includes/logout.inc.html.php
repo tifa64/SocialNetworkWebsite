9f08e234603f160ac2877748023fc6fe956b598e
muhammadkorra <muhammadkorra@gmail.com>
2017-12-11 22:02:05 +0200

    Delete logout.inc.html.php

--- includes/logout.inc.html.php
+++
@@ -1,7 +0,0 @@
-<form action="" method="post">
-  <div>
-    <input type="hidden" name="action" value="logout">
-    <input type="hidden" name="goto" value="/admin/">
-    <input type="submit" name ="action" value="Logout">
-  </div>
-</form>
