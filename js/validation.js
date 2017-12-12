5fc357567c39152be18601bb01fb7b133e82b0ff
AbdullahDiDo <abdullahabdelwahab007@gmail.com>
2017-12-12 02:12:24 +0200

    completed front-end

---
+++ js/validation.js
@@ -0,0 +1,83 @@
+var g_signup =0;
+var g_login =1;
+show_hide_signup();
+show_hide_login();
+function validatelogin(){
+        var username1 = document.getElementById("username1").value;
+        var password1 = document.getElementById("password1").value;
+        if(username1 == null || username1 == ""){
+            alert("Enter your username ! ");
+            return false;
+        }
+        else if(password1 == null || password1  == ""){
+            alert("Enter your password ! ");
+            return false;
+        }
+        else return true;
+    }
+function checkUsername_pass() {
+    var Msg = document.getElementById('feedback_user');
+    var Msg4 = document.getElementById('feedback_user1');
+    var Msg2= document.getElementById('feedback_pass');
+    var Msg3= document.getElementById('feedback_email');
+    var Firstname = document.getElementById('firstname');
+    var Lastname = document.getElementById('lastname');
+    var Nickname = document.getElementById('nickname');
+    var Password = document.getElementById('password');
+    var pass =1 ;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})$/;
+    var email = document.getElementById('email');
+    if (Firstname.value.length < 5 && Firstname.value != "") {
+        Msg.textContent = 'Firstname must be 5 characters or more';
+        pass = 0 ;
+    } else {
+        Msg.textContent = '';
+        pass =1 ;
+    }
+    if (Lastname.value.length < 5 && Lastname.value != "") {
+        Msg4.textContent = 'Lastname must be 5 characters or more';
+        pass = 0 ;
+    } else {
+        Msg4.textContent = '';
+        pass =1 ;
+    }
+    if (Password.value.length < 5 && Password.value !="") {
+        Msg2.textContent = 'Password must be 5 characters or more';
+        pass =0 ;
+    } else {
+        Msg2.textContent = '';
+        pass =1 ;
+    }
+    if (!filter.test(email.value) && email.value != "") {
+        Msg3.textContent = 'invalid email !';
+        pass =0;
+    }
+    else {
+        Msg3.textContent ='';
+        pass = 1 ;
+    }
+    if (!pass){
+        return false ;
+    }
+    else {
+        return true ;
+    }
+}
+function show_hide_signup(){
+    if(g_signup == 0){
+    document.getElementById('signup').style.display="none";
+        g_signup =1 ;}
+    else {
+        document.getElementById('signup').style.display="block";
+        g_signup=0;
+    }
+}
+function show_hide_login(){
+    if(g_login == 0){
+        document.getElementById('login').style.display="none";
+        g_login =1 ;}
+    else {
+        document.getElementById('login').style.display="block";
+        g_login=0;
+    }
+}
---
+++ registration.html.php
@@ -0,0 +1,58 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] .
+    '/includes/helpers.inc.php'; ?>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Social Network System</title>
+  <!-- <link rel="stylesheet" href="css/style.css" /> -->
+</head>
+<body>
+<div id ="page">
+        <h1> Social Network System </h1>
+        <button onclick="show_hide_login()" id="login_button">login?</button>
+<form  action="?login" method="post" onsubmit="return validatelogin()" id="login">
+    <?php if (isset($SignupError)): ?>
+        <p><?php htmlout($SignupError); ?></p>
+    <?php endif; ?>
+    <fieldset>
+        <legend>Login</legend>
+     <div>  <label for="email">Email:<input type="email" name="email" id="email1" value="" required > </label></div>
+       <div> <label for="password">Password:<input type="password" name="password"id="password1" value="" required></label></div>
+        <input type="submit" name="action" value="login">
+    </fieldset>
+</form>
+        <button onclick="show_hide_signup()" id="signup_button">singup?</button>
+
+<form  action ="?signup" method="post" onsubmit=""  id="signup"  onsubmit="return checkUsername_pass()">
+    <fieldset>
+        <legend>Sign up</legend>
+        <label for="firstname">Firstname *:<input type="text" name="firstname" id="firstname" value="" onblur="checkUsername_pass()" required> </label>
+        <div id="feedback_user"> </div>
+        <label for="lastname">Lastname *:<input type="text" name="lastname" id="lastname" value="" onblur="checkUsername_pass()" required> </label>
+        <div id="feedback_user1"> </div>
+        <label for="nickname">Nickname:<input type="text" name="nickname"id="nickname" value=""> </label> <br/>
+        Phone number1( xx-xxxx-xxxx) :<input id="telNo1" name="telNo1" type="tel"  pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"><br/>
+        Phone number2( xx-xxxx-xxxx) :<input id="telNo2" name="telNo2" type="tel"  pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"><br/>
+        Phone number3( xx-xxxx-xxxx) :<input id="telNo3" name="telNo3" type="tel"  pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"><br/>
+        <label for="password">Password *:<input type="password" name="password"id="password" value="" onblur="checkUsername_pass()" required> </label>
+        <div id="feedback_pass"> </div>
+        <label for="email">Email *:<input type="email" name="email" id="email" value="" onblur="checkUsername_pass()" required> </label><br/>
+        <div id="feedback_email"> </div>
+        Gender *:<input id="female" type="radio" name="gender" value="female" required><label for="female">Female</label>
+        <input id="male" type="radio" name="gender" value="male" required> <label for="male">Male</label> <br/>
+        Marital status:<input id="single" type="radio" name="status" value="single" > <label for="single">Single</label>
+        <input id="Engaged" type="radio" name="status" value="engaged"> <label for="Engaged">Engaged</label>
+        <input id="Married" type="radio" name="status" value="married"> <label for="Married">Married</label><br/>
+        <label for="Birthdate">Birthdate *:<input type="date" name="birthdate"  min="1900-01-01" max="2010-01-01" id="birthdate" required> </label> <br/>
+        <label for="Hometown">Hometown :<input type="text" name="hometown"id="hometown" value=""> </label><br/>
+        <label for="aboutme"> Tell us more about you !</label><br/>
+        <textarea name="aboutme" cols="40" rows="6" id="aboutme">Here you go ...</textarea><br/>
+        <input type="submit" name="action" value="SignUp">
+    </fieldset>
+</form>
+
+<script type="text/javascript" src="/js/validation.js" > </script>
+</body>
+</html>
