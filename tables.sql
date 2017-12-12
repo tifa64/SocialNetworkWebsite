7222fa48153851ed24b671d102205252ea6cdbf0
Karim Elawaad <karim.elawaad@hotmail.com>
2017-12-12 22:15:52 +0200

    Added null and unique constrains

--- tables.sql
+++ tables.sql
@@ -1,25 +1,25 @@
 CREATE  TABLE  user(
   user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  email  VARCHAR(255) ,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
-  password CHAR(32),
-  image_url VARCHAR(255),
-  gender ENUM('male', 'female'),
+  email  VARCHAR(255) NOT NULL UNIQUE,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  password CHAR(32) NOT NULL,
+  image_url VARCHAR(255) NOT NULL,
+  gender ENUM('male', 'female') NOT NULL,
   home_town VARCHAR(255),
   about_me TEXT ,
   nick_name VARCHAR(255),
   martial_status ENUM ('single','engaged','married'),
   birth_date DATE NOT NULL ,
-  reg_date DATE NOT NULL
+  reg_date TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
 )DEFAULT  CHARACTER SET utf8 ENGINE  =InnoDB;
 
 CREATE TABLE  posts(
   post_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  isPublic BOOLEAN,
-  user_id INT ,
-  caption TEXT,
-  time DATETIME NOT NULL ,
+  isPublic BOOLEAN NOT NULL DEFAULT 1,
+  user_id INT NOT NULL,
+  caption TEXT NOT NULL,
+  time TIMESTAMP NOT NULL default CURRENT_TIMESTAMP ,
   FOREIGN KEY (user_id) REFERENCES user (user_id)
     ON DELETE  CASCADE  ON UPDATE  CASCADE
 )DEFAULT CHARACTER SET utf8 ENGINE =InnoDB;
@@ -32,8 +32,8 @@ CREATE TABLE  phone_numbers(
 )DEFAULT CHARACTER SET utf8 ENGINE =InnoDB;
 CREATE TABLE  notifications (
   notification_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id1 INT,
-  user_id2 INT,
+  user_id1 INT NOT NULL,
+  user_id2 INT NOT NULL,
   seen BIT(1) DEFAULT 0,
   FOREIGN KEY (user_id1) REFERENCES  user(user_id)
     ON DELETE  CASCADE  ON UPDATE  CASCADE,
@@ -61,6 +61,7 @@ CREATE  TABLE emotions (
 CREATE  TABLE posts_likes (
   post_id INT NOT NULL ,
   user_id INT NOT NULL ,
+  date TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY (post_id,user_id)
 )DEFAULT CHARACTER SET utf8 ENGINE =InnoDB;
 
