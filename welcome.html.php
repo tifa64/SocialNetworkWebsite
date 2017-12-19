11f5f6eaeeb9c1df2044ddf0a8c5017fc02a1769
Karim Elawaad <karim.elawaad@hotmail.com>
2017-12-19 04:01:25 +0200

    Added welcome

---
+++ welcome.html.php
@@ -0,0 +1,22 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] .
+    '/includes/helpers.inc.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Social Network</title>
+</head>
+<body>
+<?php if (isset($_SESSION['userid'])): ?>
+    <p> WELCOME <?php htmlout($_SESSION['userid']); ?></p>
+<?php endif; ?>
+<form action="./index.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="hidden" name="userid" value="<?php htmlout($_SESSION['userid']);?>">
+
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/social-network/includes/logout.inc.html.php'; ?></p>
+</body>
+</html>
