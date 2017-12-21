2d55373e946b08e258537cbe4e36925ac2bc1cfd
tifa64 <krietallo@gmail.com>
2017-12-21 19:39:56 +0200

    Editing user's firstname

--- editprofile.html.php
+++ editprofile.html.php
@@ -5,12 +5,11 @@
 </head>
 <body>
 <form  action ="" method="post" onsubmit=""  id="editprofile">
-        <fieldset>
-            <legend>Edit profile</legend>
-            <label for="firstname">Firstname *:<input type="text" name="firstname" id="firstname" value="" onblur="" > </label>
-            
-            <input type="submit" name="action" value="editProfile">
-        </fieldset>
+	<fieldset>
+  	<legend>Edit profile</legend>
+      <label for="firstname">Firstname *:<input type="text" name="firstname" id="firstname" value="" onblur="" > </label>
+	</fieldset>
+  <input type="submit" name="action" value="editProfile">
 </form>
 </body>
 </html>
